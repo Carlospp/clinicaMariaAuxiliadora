b48707472020a9450c6db54dfcff322543aa64db
Carlos Porras <porrasc25@gmail.com>
2015-09-03 11:53:24 -0500

    segundo commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/index'
 
-  get 'pages/about'
+  root 'pages#index'
 
-  get 'pages/services'
-
-  get 'pages/contact'
+  get 'about' => 'pages#about', as: :about
+  get 'services' => 'pages#services', as: :services
+  get 'contact' => 'pages#contact', as: :contact
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
